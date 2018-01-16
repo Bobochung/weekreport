c5b3c138cbbea3b47e5c0a20b06a7d8a9666d1b1
Bobochung <zb911318@gmail.com>
2018-01-16 16:28:34 +0800

    weekreport
    start

---
+++ week_report/src/main/java/com/rimi/report/WeekReportApplication.java
@@ -0,0 +1,12 @@
+package com.rimi.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekReportApplication.class, args);
+	}
+}
