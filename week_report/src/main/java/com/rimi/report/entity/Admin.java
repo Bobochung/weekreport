43d0a31b8803fa0206d0a9867ebde82da887cefd
guannipishi <410992507@qq.com>
2018-01-19 10:44:23 +0800

    实体类

---
+++ week_report/src/main/java/com/rimi/report/entity/Admin.java
@@ -0,0 +1,36 @@
+package com.rimi.report.entity;
+
+public class Admin {
+	private int id;
+	private String admin_name;
+	private String admin_password;
+	private String admin_tel;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAdmin_name() {
+		return admin_name;
+	}
+	public void setAdmin_name(String admin_name) {
+		this.admin_name = admin_name;
+	}
+	public String getAdmin_password() {
+		return admin_password;
+	}
+	public void setAdmin_password(String admin_password) {
+		this.admin_password = admin_password;
+	}
+	public String getAdmin_tel() {
+		return admin_tel;
+	}
+	public void setAdmin_tel(String admin_tel) {
+		this.admin_tel = admin_tel;
+	}
+	
+	
+	
+
+}
---
+++ week_report/src/main/java/com/rimi/report/entity/Classes.java
@@ -0,0 +1,25 @@
+package com.rimi.report.entity;
+
+public class Classes {
+	private int id;
+	private String classes;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getClasses() {
+		return classes;
+	}
+	public void setClasses(String classes) {
+		this.classes = classes;
+	}
+	
+	
+	
+	
+
+}
