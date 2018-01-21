23f6276139fcc8b51f8f1e0efc466ded45c2444f
Bobochung <zb911318@gmail.com>
2018-01-21 17:22:02 +0800

    改表名
    admin改为admin_

--- week_report/src/test/java/com/rimi/report/WeekReportApplicationTests.java
+++ week_report/src/test/java/com/rimi/report/WeekReportApplicationTests.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.rimi.report.mapper.AdminMapper;
 import com.rimi.report.service.AdminService;
-import com.sun.tools.classfile.StackMapTable_attribute.append_frame;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
